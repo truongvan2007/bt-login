ca749ab5db5fe0d1a964d1369d226b1f70276538
truongvan2007 <truongvan2007@gmail.com>
2025-03-28 15:43:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const users = [
     { username: 'admin', password: '1' },
     { username: 'user', password: '2' }
 ];
+// Built-in middleware to parse JSON bodies
+app.use(express.json());
+
+// Built-in middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(corsOptions));
 
